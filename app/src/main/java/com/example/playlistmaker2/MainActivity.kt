dadf77259064da799f441d70343bb5071a37ac8b
Format010 <foreneng@yandex.ru>
2024-02-12 09:59:20 +0300

    Com1

--- app/src/main/java/com/example/playlistmaker2/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker2/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.playlistmaker2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.playlistmaker2.MediaActivity
+import com.example.playlistmaker2.R
+import com.example.playlistmaker2.SearchActivity
+import com.example.playlistmaker2.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<View>(R.id.search)
+        val media = findViewById<View>(R.id.media)
+        val settings = findViewById<View>(R.id.settings)
+
+       searchButton.setOnClickListener{
+           val displayIntentSearch = Intent(this, SearchActivity::class.java)
+           startActivity(displayIntentSearch)
+       }
+        media.setOnClickListener{
+            val displayIntentMedia = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntentMedia)
+        }
+        settings.setOnClickListener{
+            val displayIntentSettings = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntentSettings)
+        }
     }
 }
