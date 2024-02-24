1737c4ab1ad1e44a87f240518a2f22cdcb9948f0
Format010 <foreneng@yandex.ru>
2024-02-24 15:54:34 +0300

    SPR10

---
+++ app/src/main/java/com/example/playlistmaker2/SearchHolder.kt
@@ -0,0 +1,27 @@
+package layout
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker2.R
+import com.example.playlistmaker2.Track
+import com.bumptech.glide.Glide
+
+class SearchHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    private val rootLayout: LinearLayout = itemView.findViewById(R.id.rootLayout)
+    private val artwork: ImageView = itemView.findViewById(R.id.artwork)
+    private val track_name: TextView = itemView.findViewById(R.id.track_name)
+    private val artist_name: TextView = itemView.findViewById(R.id.artist_name)
+    private val track_time: TextView = itemView.findViewById(R.id.track_time)
+
+    fun bind(track: Track){
+        Glide.with(itemView).load(track.artworkUrl100).into(artwork)
+        track_name.text = track.trackName
+        artist_name.text = track.artistName
+        track_time.text = track.trackTime
+    }
+
+}
