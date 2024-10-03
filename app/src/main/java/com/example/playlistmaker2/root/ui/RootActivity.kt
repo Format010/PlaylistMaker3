15dac1ff65a42e06cec3fa477b3db358ebbd3af5
Format010 <foreneng@yandex.ru>
2024-10-03 15:21:00 +0300

    SPR19_1

---
+++ app/src/main/java/com/example/playlistmaker2/root/ui/RootActivity.kt
@@ -0,0 +1,38 @@
+package com.example.playlistmaker2.root.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.playlistmaker2.R
+import com.example.playlistmaker2.databinding.ActivityRootBinding
+
+class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener {_, destination, _ ->
+            when (destination.id) {
+                R.id.audioPlayerActivity -> {
+                    bottomNavigationView.visibility = View.GONE
+                }else -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+
+            }            }
+
+    }
+
+}
